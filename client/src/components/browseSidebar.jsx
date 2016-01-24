c665134641e17b057497398d27b7c47e4247632e
steele2345 <beresford211@users.noreply.github.com>
2016-01-24 11:26:52 -0800

    [refact] turned html into react format for browseSidebar

--- client/src/components/browseSidebar.jsx
+++ client/src/components/browseSidebar.jsx
@@ -2,25 +2,20 @@ import React from 'react';
 import {Link} from 'react-router';
 
 export default class BrowseSideNavbar extends React.Component {
+
   render() {
+    var category = ["Antiques", "Art", "Automobiles", "clothing", "electronics",
+    "Entertainment", "Jewelry", "Tickets and Experiences", "Travel"];
     return (
-      <nav className="navbar navbar-default">
-        <div className="container-fluid">
-          <div className="navbar-header">
-            <ul className="nav navbar-nav navbar-right">
-              <li><a href="#">Antiques & Collectibles</a></li>
-              <li><a href="#">Art</a></li>
-              <li><a href="#">Automobiles</a></li>
-              <li><a href="#">Clothing, Shoes & Accessories</a></li>
-              <li><a href="#">Electronics</a></li>
-              <li><a href="#">Entertainment Memorabilia</a></li>
-              <li><a href="#">Jewelry & Watches</a></li>
-              <li><a href="#">Tickets & Experiences</a></li>
-              <li><a href="#">Travel</a></li>
-            </ul>
-          </div>
+      <div className="row">
+         <div className="col-sm-3 col-md-2 sidebar">
+          <ul className="nav nav-sidebar browse">
+              {category.map(function(subCategory, index) {
+                return <li key={index}><a href={index}>{subCategory}</a></li>;
+              })}
+          </ul>
         </div>
-      </nav>
+      </div>
     );
   }
 };
