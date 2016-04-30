1c01bb4af4fe071002c95d9b575712c1ee7f8e3b
steele2345 <beresford211@users.noreply.github.com>
2016-04-29 21:01:53 -0700

    add: includes chutte video

---
+++ client/src/components/videoExplanation.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const VideoExplanation = function() {
+  return (
+    <div>
+      <iframe width="560" height="315" src="https://www.youtube.com/embed/L0rUpUCV1jk" frameborder="0" allowfullscreen></iframe>
+    </div>
+  )
+};
+
+export default VideoExplanation;
