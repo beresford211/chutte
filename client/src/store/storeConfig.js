9b04ca68ea4c594e559c734f8fac92284949fd1f
steele2345 <beresford211@users.noreply.github.com>
2016-06-13 15:21:54 -0700

    update

--- client/src/store/storeConfig.js
+++ client/src/store/storeConfig.js
@@ -2,6 +2,6 @@ if (process.env.NODE_ENV === 'production') {
   console.log("production testing")
   module.exports = require('./storeConfig.prod')
 } else {
-  console.log("dev testing")
+  console.log("dev testing", process.env.NODE_ENV)
   module.exports = require('./storeConfig.dev')
 }
