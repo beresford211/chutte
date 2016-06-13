dbe5ad8428c06f35e86d1d48b231a29a0e0755a6
steele2345 <beresford211@users.noreply.github.com>
2016-06-13 14:48:09 -0700

    updated bundle

--- client/public/dist/bundle.js
+++ client/public/dist/bundle.js
@@ -39189,11 +39189,11 @@
 	
 	function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 	
-	// const DevTools = createDevTools(
-	//   <DockMonitor toggleVisibilityKey="shift-ctrl-h" changePositionKey="shift-ctrl-q">
-	//     <LogMonitor />
-	//   </DockMonitor>
-	//   );
+	var DevTools = (0, _reduxDevtools.createDevTools)(_react2.default.createElement(
+	  _reduxDevtoolsDockMonitor2.default,
+	  { toggleVisibilityKey: 'shift-ctrl-h', changePositionKey: 'shift-ctrl-q' },
+	  _react2.default.createElement(_reduxDevtoolsLogMonitor2.default, null)
+	));
 	
 	exports.default = DevTools;
 	
--- client/src/containers/devTools.jsx
+++ client/src/containers/devTools.jsx
@@ -3,10 +3,10 @@ import { createDevTools } from 'redux-devtools';
 import LogMonitor from 'redux-devtools-log-monitor';
 import DockMonitor from 'redux-devtools-dock-monitor';
 
-// const DevTools = createDevTools(
-//   <DockMonitor toggleVisibilityKey="shift-ctrl-h" changePositionKey="shift-ctrl-q">
-//     <LogMonitor />
-//   </DockMonitor>
-//   );
+const DevTools = createDevTools(
+  <DockMonitor toggleVisibilityKey="shift-ctrl-h" changePositionKey="shift-ctrl-q">
+    <LogMonitor />
+  </DockMonitor>
+  )
 
 export default DevTools;
